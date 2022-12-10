56c95a1908ef2d5e80eb181c0f040f1de64010f4
Pranav Waghmare <piyushw55@gmail.com>
2022-12-10 20:06:40 +0530

    Multiplication Table

---
+++ Numbers_in_Csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Numbers_in_Csharp
+{
+    internal class Program
+    {
+        public static void DisplayTable(int iNo)
+        {
+            int iCnt = 0;
+            if (iNo < 0)
+            {
+                iNo = -iNo;
+            }
+            for (iCnt = 1; iCnt <= 10; iCnt++)
+            {
+                Console.WriteLine(iNo+" * "+iCnt+" = "+iNo*iCnt);       
+            }
+        }
+        static void Main(string[] args)
+        {
+            int iValue = 0;
+            Console.WriteLine("Enter number :");
+            iValue = Convert.ToInt32(Console.ReadLine());
+
+            DisplayTable(iValue);
+        }
+    }
+}
